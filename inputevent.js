93d9980c04c6cbbbf043ffdc8a4e194434c47e83
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-10 23:55:36 +0530

    Added new files

--- inputevent.js
+++ inputevent.js
@@ -1,8 +1,18 @@
 // use input eventlistner in this function
-let input= document.querySelector("input");
+let input=document.querySelector("input");
+
+function inputevent( event){
+
+    if(event.data!==null){
+
+        console.log(event.data);
+    };
+};
+input.addEventListener("input",inputevent);
+/*let input= document.querySelector("input");
  function inpt( event ){
     if(event.data!==null){
     console.log( event.data  );
     };
  };
- input.addEventListener("input",inpt);
+ input.addEventListener("input",inpt);*/
