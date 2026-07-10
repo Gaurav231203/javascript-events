bf15d29382dfb976b070789b1652b28da6164c4a
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-10 23:03:32 +0530

    Added new files

---
+++ inputevent.js
@@ -0,0 +1,8 @@
+// use input eventlistner in this function
+let input= document.querySelector("input");
+ function inpt(){
+    console.log( "typed");
+
+ };
+ input.addEventListener("input",inpt);
+ 
