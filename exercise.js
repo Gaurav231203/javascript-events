3d1ecfb8e4d43fd7518f9fd15a0deb3d268b8b3c
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-11 16:47:40 +0530

    Added new files

---
+++ exercise.js
@@ -0,0 +1,18 @@
+//koi bhi key press kro wo display pr print hona chye 
+let h1=document.querySelector("h1");
+
+
+function changekey(dets){
+     if(dets.key===" "){
+        h1.textContent="you pressed space ";
+     }
+     else{
+        h1.textContent="you pressed "+`${dets.key}`;
+
+     }
+
+    //h1.textContent="you presed "+ dets.key;
+
+    
+};
+ window.addEventListener("keydown",changekey);
