d12ab86fc71349e5d175570dc8b3da94d738989e
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-16 13:10:05 +0530

    Added new files

--- event-bubbling.js
+++ event-bubbling.js
@@ -4,8 +4,16 @@ nav.addEventListener("click",function(){
 })*/
 
 //this is the example of event bubbling
-let line=document.querySelector("ul")
-line.addEventListener("click", function(events) {
-    events.target.classList.toggle("lt")//we used toggle to remove linethrough 
-    
-});
+
+//step1=>html element select kro
+let ul=document.querySelector("ul");
+//step2=> selected eleemnt pr event lagao 
+ ul.addEventListener("click", function(dets){
+    //alert("clicked")
+    //step3=> abb jo bhi tumko perform krwana hai use kro jese jo function kaya andr parameter diya hai 
+    // dets likha hai uss say hume ky krwana hai 
+   dets.target.classList.toggle("lt")
+   //yaha pr maine ul li jo details thi usami target kiya hrr ek list ko or uspr this.classList toggel krwaya
+
+ })
+
